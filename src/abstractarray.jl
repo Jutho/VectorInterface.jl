4299f0c1fc43a819c8b3c4dca89b2653c5061aef
Jutho Haegeman <jutho.haegeman@ugent.be>
2022-10-27 22:42:50 +0200

    simplify implementation for `AbstractArray'

--- src/abstractarray.jl
+++ src/abstractarray.jl
@@ -51,39 +51,27 @@ function scale!(x::AbstractArray, α::Number)
     x .= scale!!.(x, α)
     return x
 end
+function scale!(y::AbstractArray{T}, x::AbstractArray, α::Number) where {T<:BlasFloat}
+    LinearAlgebra.mul!(y, x, convert(T, α))
+    return y
+end
 function scale!(y::AbstractArray, x::AbstractArray, α::Number)
     y .= scale!!.(y, x, α)
     return y
 end
 
-function scale!!(x::AbstractArray{T}, α::T) where {T<:BlasFloat}
-    LinearAlgebra.rmul!(x, α)
-    return x
-end
-function scale!!(x::AbstractArray{T}, α::T) where {T<:Number}
-    x .*= α
-    return x
-end
-function scale!!(x::AbstractArray{T}, α::Number) where {T<:Number}
-    if promote_type(T, typeof(α)) <: T
-        return scale!!(x, convert(T, α))
-    else
-        return scale!!.(x, α)
-    end
-end
 function scale!!(x::AbstractArray, α::Number)
     T = scalartype(x)
     if promote_type(T, typeof(α)) <: T
-        x .= scale!!.(x, α)
+        return scale!(x, α)
     else
         return scale!!.(x, α)
     end
 end
 function scale!!(y::AbstractArray, x::AbstractArray, α::Number)
-    T = scalartype(x)
-    if promote_type(T, typeof(α)) <: T
-        y .= scale!!.(y, x, α)
-        return y
+    T = scalartype(y)
+    if promote_type(T, typeof(α), scalartype(x)) <: T
+        return scale!(y, x, α)
     else
         return scale!!.(y, x, α)
     end
@@ -98,7 +86,7 @@ function add(y::AbstractArray, x::AbstractArray, α::ONumber = _one, β::ONumber
     return add.(y, x, (α,), (β,))
 end
 
-# Special case simple numerical arrays with BLAS-compatible floating point type
+# Special case: simple numerical arrays with BLAS-compatible floating point type
 function add!(y::AbstractArray{T}, x::AbstractArray{T},
                 α::ONumber = _one, β::_One = _one) where {T<:BlasFloat}
     LinearAlgebra.axpy!(convert(T, α), x, y)
@@ -109,7 +97,7 @@ function add!(y::AbstractArray{T}, x::AbstractArray{T},
     LinearAlgebra.axpby!(convert(T, α), x, convert(T, β), y)
     return y
 end
-# Generic implementation: no 'try catch' block for performance, but might error ungracefully
+# General case:
 function add!(y::AbstractArray, x::AbstractArray, α::ONumber = _one, β::ONumber = _one)
     ax = axes(x)
     ay = axes(y)
@@ -118,42 +106,14 @@ function add!(y::AbstractArray, x::AbstractArray, α::ONumber = _one, β::ONumbe
     return y
 end
 
-# Special case simple numerical arrays with BLAS-compatible floating point type
-function add!!(y::AbstractArray{T}, x::AbstractArray{T},
-                α::ONumber = _one, β::_One = _one) where {T<:BlasFloat}
-    if promote_type(T, typeof(α)) <: T
-        y = add!(y, x, convert(T, α))
-        return y
-    else
-        ax = axes(x)
-        ay = axes(y)
-        ax == ay || throw(DimensionMismatch("Output axes $ay differ from input axes $ax"))
-        return add!!.(y, x, (α,))
-    end
-end
-function add!!(y::AbstractArray{T}, x::AbstractArray{T},
-                α::ONumber, β::Number) where {T<:BlasFloat}
-    if promote_type(T, typeof(α)) <: T && promote_type(T, typeof(β)) <: T
-        y = add!(y, x, convert(T, α), convert(T, β))
-        return y
-    else
-        ax = axes(x)
-        ay = axes(y)
-        ax == ay || throw(DimensionMismatch("Output axes $ay differ from input axes $ax"))
-        return add!!.(y, x, (α,), (β,))
-    end
-end
-
-# Generic implementation: with 'try catch' block to avoid errors
 function add!!(y::AbstractArray, x::AbstractArray, α::ONumber = _one, β::ONumber = _one)
-    ax = axes(x)
-    ay = axes(y)
-    ax == ay || throw(DimensionMismatch("Output axes $ay differ from input axes $ax"))
     T = scalartype(y)
     if promote_type(T, typeof(α), typeof(β), scalartype(x)) <: T
-        y .= add!!.(y, x, (α,), (β,))
-        return y
+        return add!(y, x, α, β)
     else
+        ax = axes(x)
+        ay = axes(y)
+        ax == ay || throw(DimensionMismatch("Output axes $ay differ from input axes $ax"))
         return add!!.(y, x, (α,), (β,))
     end
 end
