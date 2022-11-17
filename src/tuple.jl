4796bd551fd3a269a124fa6d22fc9c1fef7e10e4
Jutho Haegeman <jutho.haegeman@ugent.be>
2022-11-17 15:14:44 +0100

    improved tuple implementation

--- src/tuple.jl
+++ src/tuple.jl
@@ -9,7 +9,7 @@ function scalartype(::Type{Tuple{}})
     throw(ArgumentError("no scalar type is defined for empty tuple"))
 end
 scalartype(::Type{Tuple{T}}) where {T} = scalartype(T)
-scalartype(::Type{Tuple{T,Vararg{T}}}) where {T} = scalartype(T)
+scalartype(::Type{<:Tuple{T,Vararg{T}}}) where {T} = scalartype(T)
 function scalartype(::Type{TT}) where {TT<:Tuple}
     S = scalartype(Base.tuple_type_head(TT))
     S2 = scalartype(Base.tuple_type_tail(TT))
@@ -33,12 +33,11 @@ end
 scale(x::Tuple, α::ONumber) = map(xᵢ -> scale(xᵢ, α), x)
 scale!!(x::Tuple, α::ONumber) = map(xᵢ -> scale!!(xᵢ, α), x)
 function scale!!(y::Tuple, x::Tuple, α::ONumber)
-    if length(y) == length(x)
-        yx = ntuple(i -> (y[i], x[i]), length(x))
-        return map(yxᵢ -> scale!!(yxᵢ[1], yxᵢ[2], α), yx)
-    else
-        return scale(x, α)
-    end
+    lx = length(x)
+    ly = length(y)
+    lx == ly || throw(DimensionMismatch("non-matching tuple lengths $lx and $ly"))
+    yx = ntuple(i -> (y[i], x[i]), lx)
+    return map(yxᵢ -> scale!!(yxᵢ[1], yxᵢ[2], α), yx)
 end
 
 # add & add!!
@@ -46,16 +45,14 @@ end
 function add(y::Tuple, x::Tuple, α::ONumber=_one, β::ONumber=_one)
     lx = length(x)
     ly = length(y)
-    lx == ly ||
-        throw(DimensionMismatch("Output tuple length $ly differs from input tuple length $lx"))
+    lx == ly || throw(DimensionMismatch("non-matching tuple lengths $lx and $ly"))
     yx = ntuple(i -> (y[i], x[i]), lx)
     return map(yxᵢ -> add(yxᵢ[1], yxᵢ[2], α, β), yx)
 end
 function add!!(y::Tuple, x::Tuple, α::ONumber=_one, β::ONumber=_one)
     lx = length(x)
     ly = length(y)
-    lx == ly ||
-        throw(DimensionMismatch("Output tuple length $ly differs from input tuple length $lx"))
+    lx == ly || throw(DimensionMismatch("non-matching tuple lengths $lx and $ly"))
     yx = ntuple(i -> (y[i], x[i]), lx)
     return map(yxᵢ -> add!!(yxᵢ[1], yxᵢ[2], α, β), yx)
 end
@@ -65,7 +62,7 @@ end
 function inner(x::Tuple, y::Tuple)
     lx = length(x)
     ly = length(y)
-    lx == ly || throw(DimensionMismatch("Non-matching tuple lengths $lx and $ly"))
+    lx == ly || throw(DimensionMismatch("non-matching tuple lengths $lx and $ly"))
     T = promote_type(scalartype(x), scalartype(y))
     xy = ntuple(i -> (x[i], y[i]), lx)
     return sum(map(xyᵢ -> inner(xyᵢ[1], xyᵢ[2]), xy))
