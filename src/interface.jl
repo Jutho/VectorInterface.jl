378c3d3adf24b3ed3c2ac6baaf4e7535011015b9
lkdvos <lukas.devos@ugent.be>
2023-09-06 12:03:25 +0200

    Update docstrings

--- src/interface.jl
+++ src/interface.jl
@@ -97,6 +97,11 @@ function scale!! end
 
 Add `y` and `x`, or more generally construct the linear combination `y * β + x * α`.
 
+!!! note
+    New types should only implement the four-argument version. When desired, the two- and
+    three-argument version can be distinguished by specializing on `α` and `β` being of
+    type `One`.
+
 See also: [`add!`](@ref) and [`add!!`](@ref)
 """
 add(y, x) = add(y, x, One(), One())
@@ -110,8 +115,8 @@ the result in `y`. This will error in case of incompatible scalar types or incom
 
 !!! note
     New types should only implement the four-argument version. When desired, the two- and
-    three-argument version can be distinguished by specializing on `α` and `β` being
-    `One()`.
+    three-argument version can be distinguished by specializing on `α` and `β` being of
+    type `One`.
 
 See also: [`add`](@ref) and [`add!!`](@ref)
 """
@@ -127,8 +132,8 @@ immutability, incompatible scalar types or incommensurate sizes.
 
 !!! note
     New types should only implement the four-argument version. When desired, the two- and
-    three-argument version can be distinguished by specializing on `α` and `β` being
-    `One()`.
+    three-argument version can be distinguished by specializing on `α` and `β` being of
+    type `One`.
 
 See also: [`add`](@ref) and [`add!`](@ref)
 """
