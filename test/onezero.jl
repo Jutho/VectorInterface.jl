351ad27348e81df6f883d6aa22216f330362929b
lkdvos <lukas.devos@ugent.be>
2023-09-07 17:58:23 +0200

    Format

--- test/onezero.jl
+++ test/onezero.jl
@@ -5,7 +5,8 @@ using VectorInterface
 
 const Z = Zero()
 const I = One()
-const typelist = (Int32, Int64, Float16, Float32, Float64, ComplexF16, ComplexF32, ComplexF64, BigFloat)
+const typelist = (Int32, Int64, Float16, Float32, Float64, ComplexF16, ComplexF32,
+                  ComplexF64, BigFloat)
 
 @testset "equalities" begin
     @test I == 1
@@ -13,7 +14,7 @@ const typelist = (Int32, Int64, Float16, Float32, Float64, ComplexF16, ComplexF3
     @test I != Z
     @test I == I
     @test Z == Z
-    
+
     @test iszero(Z) == true
     @test isone(I) == true
     @test isone(Z) == false
@@ -31,17 +32,17 @@ end
     @test @inferred(I - I) === Z
     @test @inferred(I - Z) === I
     @test @inferred(Z - I) == -I
-    
+
     @test @inferred(Z * Z) === Z
     @test @inferred(I * Z) === Z
     @test @inferred(Z * I) === Z
     @test @inferred(I * I) === I
-    
+
     @test @inferred(Z / I) === Z
     @test @inferred(I / I) === I
     @test_throws DivideError @inferred(I / Z)
     @test_throws DivideError @inferred(Z / Z)
-    
+
     for T in typelist
         x = rand(T)
         while iszero(x)
@@ -52,17 +53,17 @@ end
         @test @inferred(x + Z) == x
         @test @inferred(I + x) == x + 1
         @test @inferred(x + I) == x + 1
-        
+
         @test @inferred(Z - x) == -x
         @test @inferred(x - Z) == x
         @test @inferred(I - x) == 1 - x
         @test @inferred(x - I) == x - 1
-        
+
         @test @inferred(Z * x) == zero(x)
         @test @inferred(x * Z) == zero(x)
         @test @inferred(I * x) == x
         @test @inferred(x * I) == x
-        
+
         @test_throws DivideError @inferred(x / Z)
         @test @inferred(x / I) == x
         @test @inferred(Z / x) == zero(x)
@@ -71,7 +72,6 @@ end
 end
 
 @testset "other" begin
-    
 end
 
 @testset "promotion" begin
@@ -80,7 +80,7 @@ end
         @test @inferred(promote_type(typeof(Z), T)) == T
         @test @inferred(promote_type(T, typeof(I))) == T
         @test @inferred(promote_type(T, typeof(Z))) == T
-        
+
         @test @inferred(T(I)) == one(T)
         @test @inferred(T(Z)) == zero(T)
         @test @inferred(convert(T, I)) == one(T)
