dec1944db446eb365e27647ed4bf50e9f35b417a
leburgel <lander.burgelman@ugent.be>
2023-09-08 14:07:11 +0200

    Update docstring.

--- src/onezero.jl
+++ src/onezero.jl
@@ -11,6 +11,10 @@ struct One <: Number end
     
 Singleton type for representing a hard-coded constant 0 in vector addition / linear
 combinations.
+
+```quote
+    Now I am become Zero, the destroyer of NaN. - Vishnu
+```
 """
 struct Zero <: Number end
 
