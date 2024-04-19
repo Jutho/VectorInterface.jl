8cdc20f6c2a9ea96b3b9528aae7d2b53f4f83203
lkdvos <lukas.devos@ugent.be>
2024-04-19 09:40:31 +0000

    Catch stackoverflow

--- src/fallbacks.jl
+++ src/fallbacks.jl
@@ -21,7 +21,7 @@ function scalartype(T::Type)
     # the following does not work because `eltype` is always applicable.
     # applicable(eltype, T) && return scalartype(eltype(T))
     elT = eltype(T)
-    elT === Any && throw(ArgumentError(_error_message(scalartype, T)))
+    elT === T && throw(ArgumentError(_error_message(scalartype, T)))
     return scalartype(elT)
 end
 # should this try to use `eltype` instead? e.g. scalartype(T) = scalartype(eltype(T))
